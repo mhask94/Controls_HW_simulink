6ef075c3044f05313b4383da07d4be2f4f28ac3e
mhask94 <32270030+mhask94@users.noreply.github.com>
2018-01-18 18:27:44 -0700

    Create F_param.m

---
+++ VTOL/F_param.m
@@ -0,0 +1,27 @@
+% Physical parameters of the VTOL known to the controller
+P.ml = 0.25;     % Mass of the left prop, kg
+P.mc = 1.0;      % Mass of the center, kg
+P.m3 = 0.25;     % Mass of the right prop, kg
+P.Jc = .0042;    % Inertia of center
+P.d = 0.3;       % Distance from center to mid-prop, m
+P.g = 9.81;      % Gravity, m/s**2
+P.mu = 0.1;      % Viscosity?, kg/s
+
+% parameters for animation
+P.h = 0.05;      % Height of the beam, m
+P.gap = 0.089;   % Gap between the cart and x-axis
+P.radius = 0.06; % Radius of circular part of pendulum
+P.l_lim = -1;    % Lower limit of graph axes
+P.u_lim = 1;    % Upper limit of graph axes
+
+% Initial Conditions
+P.z0 = 0.0;                % initial cart position, m
+P.theta0 = 0.0*pi/180;     % initial rod angle, rads
+P.zdot0 = 0.0;             % initial cart velocity, m/s
+P.thetadot0 = 0.0;         % initial rod angular velocity, rads/s
+
+% Simulation Parameters
+P.t_start = 0.0;  % Start time of simulation
+P.t_end = 50.0;   % End time of simulation
+P.Ts = 0.01;      % sample time for simulation
+P.t_plot = 0.1;   % the plotting and animation is updated at this rate
